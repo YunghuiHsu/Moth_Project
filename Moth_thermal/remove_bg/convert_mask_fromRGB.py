49eef54dd3d0156a0af00df62fc28ccfcb301f12
YHHsu <twyunghui@gmail.com>
2021-11-05 16:07:08 +0800

    Add files via upload

--- Moth_thermal/remove_bg/convert_mask_fromRGB.py
+++ Moth_thermal/remove_bg/convert_mask_fromRGB.py
@@ -6,16 +6,17 @@ import matplotlib as plt
 import skimage.io as io
 from skimage import io, color
 import cv2
-from PIL import Image, ImageFilter
+from PIL import Image, ImageOps
 from skimage.transform import resize
 import cv2
 
-
+root = Path('../data')
+dir_origin = root.joinpath('origin')
+imgs_origin = list(dir_origin.glob('*.png'))
+print(len(imgs_origin))
 # ===============================================================================================
 # convert_mask_from RGB
 # ===============================================================================================
-# dir_origin = Path('../crop/origin')
-# imgs_origin = list(dir_origin .glob('*.png'))
 
 # dir_mask = Path('data/label_waiting_postprocess/mask_waitinting_for_posrprocess/for_rgb_background_fill/mask')
 # imgs_mask = list(dir_mask.glob('*.png'))
@@ -50,8 +51,6 @@ import cv2
 # ==============================================================================================
 # convert_from remove.bg
 # ===============================================================================================
-dir_origin = Path('../crop/origin')
-imgs_origin = list(dir_origin.glob('*.png'))
 
 
 # file = 'rgb_background_for_fill'  # file_for_rmbg
@@ -69,9 +68,9 @@ for idx, path in enumerate(imgs_mask):
         img_rmbg = io.imread(path)  # (256, 256, 4)
         img_name = path.stem.replace('-removebg-preview', '')
 
-        ## reading  alpha channel as mask, background == 0(mask), target(moth) == 255
+        # reading  alpha channel as mask, background == 0(mask), target(moth) == 255
         mask_ = img_rmbg[..., 3]  # (256, 256)
-        ## convert all non black(0) to white(255)
+        # convert all non black(0) to white(255)
         img_mask = np.where(mask_ < 128, 0, 255).astype('uint8')
 
         save_name = img_name + '_mask_removeweb' + '.png'
@@ -79,7 +78,7 @@ for idx, path in enumerate(imgs_mask):
         io.imsave(save_path, img_mask)
         print(idx, img_name, 'saved')
 
-        ## loading original img by img_name
+        # loading original img by img_name
         path_origin = dir_origin.joinpath(img_name + '.png')
         img_origin = io.imread(path_origin)
 
@@ -87,7 +86,7 @@ for idx, path in enumerate(imgs_mask):
         err_name.append(img_name)
         print(err, idx, img_name)
 
-## check error name
+# check error name
 print(err_name)
 
 # if file == 'file_for_rmbg':
@@ -122,8 +121,6 @@ print(err_name)
 # > 以灰階讀取，得到主體為白色(== 255)、背景非白(!=255)的影像
 # >  將非白的畫素全部轉為黑色(0)，即得到mask
 
-# dir_origin = Path('../crop/origin')
-# imgs_origin = list(dir_origin .glob('*.png'))
 
 # ## dir_mask = './bk_mask_manul/'
 # dir_mask = Path('data/label_waiting_postprocess/mask_waitinting_for_posrprocess/for_smart_rmbg/mask')
@@ -168,13 +165,11 @@ print(err_name)
 # 將所有mask [0 or 255], uint8, 單通道(channel=0)的，結合原圖、產出背景為藍色的去背影像
 # ============================================================================================
 
-dir_origin = Path('../crop/origin')
-# imgs_origin = list(dir_origin .glob('*.png'))
 
 ## dir_mask = './bk_mask_manul/'
 dir_mask = Path(
     'data\label_waiting_postprocess\mask_waitinting_for_posrprocess\mask_for_rmbg')
-    
+
 imgs_mask = list(dir_mask.glob('*.png'))
 print(len(imgs_mask))
 # imgs_mask = [path for path in dir_mask.iterdir() if "_mask" in path.stem]
@@ -209,7 +204,7 @@ for idx, path in enumerate(imgs_mask):
     # get path
     msk_path = path
     fname = path.stem.split('_cropped')[0] + '_cropped'
-    
+
     # prepare data to [0,255], channel=3
     img_path = dir_origin.joinpath(fname + '.png')
     origin_img = io.imread(img_path)
@@ -226,12 +221,13 @@ for idx, path in enumerate(imgs_mask):
     if not mask.max() > 1:
         mask = mask*255
     mask = mask.astype(np.uint8)
-    mask3 = np.stack([mask]*3, axis=2) # Create 3-channel alpha mask (h,w) > (h,w,3)。  np.dstack([mask]*3)
+    # Create 3-channel alpha mask (h,w) > (h,w,3)。  np.dstack([mask]*3)
+    mask3 = np.stack([mask]*3, axis=2)
 
-    ## get image with background removed and fill with specified color
+    # get image with background removed and fill with specified color
     img_rmbg = img_rmbg_fill(mask3, origin_img, color='blue')
 
-    ## save image
+    # save image
     img_rmbg_name = path.stem.replace('mask', 'rmbg')
     save_path_rmbg = dir_save.joinpath(img_rmbg_name + '.png')
     io.imsave(save_path_rmbg, img_rmbg)
@@ -241,14 +237,13 @@ for idx, path in enumerate(imgs_mask):
     print(idx, fname, 'saved')
 
 
-
 # ============================================================================================
 # 將mask做膨脹-侵蝕、邊緣平滑、改善邊緣缺失
 # ============================================================================================
 
-dir_origin = Path('../crop/origin')
 
-dir_mask = Path('data/label_waiting_postprocess/mask_waitinting_for_posrprocess/mask_for_dilate')
+dir_mask = Path(
+    'data/label_waiting_postprocess/mask_waitinting_for_posrprocess/mask_for_dilate')
 imgs_mask = list(dir_mask.glob('*.png'))
 print(f'file size : {len(imgs_mask)}')
 
@@ -266,7 +261,7 @@ for idx, path in enumerate(imgs_mask):
     # get path
     msk_path = path
     fname = path.stem.split('_cropped')[0] + '_cropped'
-    
+
     mask = io.imread(msk_path, as_gray=True)
 
     # dilate & erode ------------------------------------------------------------
@@ -274,7 +269,7 @@ for idx, path in enumerate(imgs_mask):
     ksize = (3, 3)
     kernel_ELLIPSE = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ksize=ksize)
     mask = cv2.dilate((mask), kernel_ELLIPSE, iterations=iterations)
-    mask  = cv2.erode(mask, kernel_ELLIPSE, iterations=(iterations-1))
+    mask = cv2.erode(mask, kernel_ELLIPSE, iterations=(iterations-1))
     # ----------------------------------------------------------------------------
 
     # smooth ------------------------------------------------------------
@@ -285,17 +280,17 @@ for idx, path in enumerate(imgs_mask):
     # mask_smooth.save(dir_save.joinpath(img_mask_name+ '_smooth' +'.png'))
     # ----------------------------------------------------------------------------
 
-    ## save mask
+    # save mask
     img_mask_name = path.stem + '_dilate' + '.png'
     save_path_mask = dir_save.joinpath(img_mask_name)
     io.imsave(save_path_mask, mask)
 
     print(idx, img_mask_name, 'saved')
 
-    ## loading original img by img_name
+    # loading original img by img_name
     # img_path = dir_origin.joinpath(fname + '.png')
     # origin_img = io.imread(img_path)
-    ## save image
+    # save image
     # io.imsave(dir_save.joinpath(fname + '.png'), origin_img)
     # print(idx, fname, 'saved')
 
@@ -305,7 +300,7 @@ for idx, path in enumerate(imgs_mask):
 # ============================================================================================
 
 
-dir_mask = Path('data\label_waiting_postprocess\mask_211101')
+dir_mask = Path('../data/label_waiting_postprocess/tmp')
 
 dir_mask_tmp = dir_mask.joinpath('masks_tmp')
 dir_mask_tmp.mkdir(parents=True, exist_ok=True)
@@ -320,20 +315,19 @@ for i, path in enumerate(dir_mask.glob('*.png')):
     mask_bi = np.where(mask_ > 128, 255, 0).astype('uint8')
     save_path = dir_mask_tmp.joinpath(mask_name + '.png')
     io.imsave(save_path, mask_bi)
-    print(i, mask_name, 'saved' )
+    print(i, mask_name, 'saved')
 
 dir_img = dir_mask.joinpath('imgs')
 dir_img.mkdir(exist_ok=True, parents=True)
 
 # ## prepare dir_img by  dir_mask
-dir_ori = Path('../crop/origin/')
+dir_ori = Path('../data/data_resize_cropped/origin/')
 for i, path in enumerate(dir_mask.iterdir()):
     if not path.name.endswith('.png'):
         continue
-    img_name = path.stem
+    img_name = path.stem.split('_mask')[0]
     origin_file = dir_ori.joinpath(img_name + '.png')
     img = io.imread(origin_file)
-    save_path = dir_img.joinpath(img_name  + '.png')
+    save_path = dir_img.joinpath(img_name + '.png')
     io.imsave(save_path, img)
-    print(i, img_name, 'saved' )
-
+    print(i, img_name, 'saved')
